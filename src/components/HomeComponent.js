f2e732da7e4510f07194d3481d6c10f4558eefe4
Natalia Prieto <natyprietoc@hotmail.com>
2021-04-24 15:18:03 -0500

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle} from 'reactstrap';
 
-const Home = () => {
+const Home = (props) => {
+    const {dish, promotion, leader} = props
+
+    const renderCard = (item) => {
+        return(
+            <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+            <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+        );
+    }
+
     return(
         <div className="container">
-            <h4>Home</h4>
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    {dish ? renderCard(dish) : ''}                    
+                </div>
+                <div className="col-12 col-md m-1">
+                    {promotion ? renderCard(promotion) : ''}
+                </div>
+                <div className="col-12 col-md m-1">
+                    {leader ? renderCard(leader) : ''}
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,16 +1,23 @@
 import React from "react";
 import HomeComponent from "./HomeComponent";
 import MenuComponent from "./MenuComponent";
+import ContactComponent from './ContactComponent';
 import DishdetailComponent from "./DishdetailComponent";
 import HeaderComponent from "./HeaderComponent";
 import FooterComponent from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 function Main() {
   const HomePage = () => {
     return(
-        <HomeComponent />
+        <HomeComponent dish={DISHES.filter((value) => value.featured)[0]}
+        promotion={PROMOTIONS.filter((value) => value.featured)[0]}
+        leader={LEADERS.filter((value) => value.featured)[0]}
+        />
     );
   }
   
@@ -20,6 +27,7 @@ function Main() {
       <Switch>
         <Route path="/home" component={HomePage} />
         <Route exact path="/menu" component={() => <MenuComponent inform={DISHES} />}/>
+        <Route exact path="/contactus" component={ContactComponent} />
         <Redirect to="/home" />
       </Switch>      
       <FooterComponent />
