061de18702beb6676a8fa2c95fdc989ae35caff1
Natalia Prieto <natyprietoc@hotmail.com>
2021-05-06 15:35:48 -0500

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,18 @@
-import { createStore } from 'redux';
-import { Reducer } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dish';
+import { Comments } from './comment';
+import { Promotions } from './promotion';
+import { Leaders } from './leader';
 
+// asi es como combinamos los reducers 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer        
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })        
     )
     return store;
 }
